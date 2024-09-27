cfa5c41ada699b30946527262c7199ea473e665d
Chirill Ciumeica <kirillchumeika@list.ru>
2024-09-27 19:19:07 +0300

    Orders

--- app/(dashboard)/[storeid]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/products/[productId]/page.tsx
@@ -37,10 +37,10 @@ const ProductPage = async ({
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
                 <ProductForm 
+                    initialData={product}
                     categories={categories}
                     colors={colors}
                     sizes={sizes}
-                    initialData={product}
                 />
             </div>
         </div>
